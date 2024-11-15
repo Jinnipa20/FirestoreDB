52c06f3ff0cb8f9af585398924dff1fb8755e5f5
Jinnipa20 <jinnipa.ass@dome.tu.ac.th>
2024-11-15 12:05:56 +0700

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiFirestoreAssignment;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		//MainPage = new AppShell();
+		MainPage = new StudentPage();
+	}
+}
